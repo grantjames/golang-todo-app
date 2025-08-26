f78ed4f1e9cec2162b8da2cc92a8224c428fbc25
Grant <grantjames@live.co.uk>
2025-08-26 23:25:59 +0100

    JSON, routing and embedding

--- test-app/in_memory_player_store.go
+++ test-app/in_memory_player_store.go
@@ -1,6 +1,8 @@
 package main
 
-import "sync"
+import (
+	"sync"
+)
 
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
 	return &InMemoryPlayerStore{
@@ -25,4 +27,12 @@ func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	i.lock.Lock()
 	defer i.lock.Unlock()
 	return i.store[name]
+}
+
+func (i *InMemoryPlayerStore) GetLeague() []Player {
+	var league []Player
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
+	}
+	return league
 }
